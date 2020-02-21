323d1e82c7f1167c19318aa0eae06830b5a1677a
evan bechtol <evanbechtol@gmail.com>
2020-02-21 09:01:19 -0600

    User Model Interaction on Google Auth
    - Find & Save user when authentication successful

--- loaders/Passport.js
+++ loaders/Passport.js
@@ -1,6 +1,9 @@
 const passport = require( "passport" );
 const GoogleStrategy = require( "passport-google-oauth20" ).Strategy;
 const { googleConsumerKey, googleConsumerSecret } = require( "../config" );
+const UserModel = require( "../models/users" );
+const MongooseService = require( "../services/MongooseService" );
+const User = new MongooseService( UserModel );
 
 // Use the GoogleStrategy within Passport.
 //   Strategies in passport require a `verify` function, which accept
@@ -14,5 +17,27 @@ const options = {
 
 passport.use( new GoogleStrategy( options,
   ( token, tokenSecret, profile, done ) => {
-    // passport callback function
+    const findQuery = { googleId: profile.id };
+
+    User.findOne( findQuery )
+      .then( currentUser => {
+        if ( currentUser ) {
+
+        } else {
+          const userData = { username: profile.displayName, googleId: profile.id };
+          const user = new UserModel( userData );
+
+          User.create( user )
+            .then( newUser => {
+              console.log( `New user created: ${newUser}` );
+            } )
+            .catch( err => {
+              console.error( err );
+            } );
+        }
+      } )
+      .catch( err => console.error( err ) );
+
+
   } ) );
+
--- models/users.js
+++ models/users.js
@@ -3,6 +3,7 @@ const mongoose = require( 'mongoose' );
 const userSchema = mongoose.Schema( {
   username: {
     type: String,
+    unique: true,
     required: true
   },
 
