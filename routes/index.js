668fdf6cb47fe2e5d373129511ec46acba34e52c
eevabec <evan.bechtol@ericsson.com>
2020-02-22 16:20:42 -0600

    Update Access-Control-Allow-Origin header
    - Set to localhost:8080 to work with UI
    
    Signed-off-by: eevabec <evan.bechtol@ericsson.com>

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const user = require( "./user" );
 
 const routes = app => {
   app.use( ( req, res, next ) => {
-    res.setHeader( "Access-Control-Allow-Origin", "*" );
+    res.setHeader( "Access-Control-Allow-Origin", "http://localhost:8080" );
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
