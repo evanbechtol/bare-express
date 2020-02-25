a5dc541d61a4db1268c0086d29435949e8ab18e3
eevabec <evan.bechtol@ericsson.com>
2020-02-25 15:43:32 -0600

    Create `/auth/github` routes
    - Create `github.js`
    
    Signed-off-by: eevabec <evan.bechtol@ericsson.com>

---
+++ routes/auth/github.js
@@ -0,0 +1,14 @@
+const express = require( "express" );
+const router = express.Router();
+const passport = require( "passport" );
+
+router.get( "/", passport.authenticate( "github", {
+  scope: [ "email" ]
+} ) );
+
+router.get( "/redirect", passport.authenticate( "github", {
+  successRedirect: "http://localhost:8080/profile",
+  failureRedirect: "http://localhost:8080/login"
+} ) );
+
+module.exports = router;
