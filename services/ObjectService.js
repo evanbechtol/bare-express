2ace96e115c6820f1c70241d2a664ddd10fe3fda
Evan Bechtol <evanbechtol@gmail.com>
2022-04-05 14:02:10 -0500

    Sort users

--- services/ObjectService.js
+++ services/ObjectService.js
@@ -1,16 +1,23 @@
-class ObjectService {
-  /**
-   * @description Remove keys which are not present on the whitelist, and
-   *   return the object with only the whitelisted keys
-   * @param obj {object} Object to filter using the whitelist
-   * @param keys {array} List of whitelisted keys
-   * @returns {object} Return object with only the whitelisted keys
-   */
-  static pick ( obj, keys ) {
+/**
+ * @description Remove keys which are not present on the whitelist, and
+ *   return the object with only the whitelisted keys
+ * @param obj {object} Object to filter using the whitelist
+ * @param keys {array} List of whitelisted keys
+ * @returns {object} Return object with only the whitelisted keys
+ */
+function pick(obj, keys) {
     return keys
-      .map( k => k in obj ? { [ k ]: obj[ k ] } : {} )
-      .reduce( ( res, o ) => Object.assign( res, o ), {} );
-  }
+        .map(k => k in obj ? {[k]: obj[k]} : {})
+        .reduce((res, o) => Object.assign(res, o), {});
 }
 
-module.exports = ObjectService;
+function sortObject(data, comparator, sort) {
+    let sortedData = [...data].sort(comparator);
+
+    if (sort.direction !== "asc") {
+        sortedData = sortedData.reverse();
+    }
+
+    return sortedData;
+}
+module.exports = {pick, sortObject};
--- services/UserService/index.js
+++ services/UserService/index.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const {stringifyQuerystring} = require("../../util/queryUtil");
+const {sortObject} = require("../ObjectService");
 const apiUrl = (queryString) => `https://randomuser.me/api/?&${queryString ? queryString : ''}`;
 
 async function getUsers(query) {
@@ -11,6 +12,23 @@ async function getUsers(query) {
         const paramsStr = stringifyQuerystring(paramsArray)
         const {data} = await axios.get(apiUrl(paramsStr));
         response = data.results;
+
+        if (query.sort) {
+            let sort = query.sort;
+
+            if (typeof sort !== 'object') {
+                try {
+                    sort = JSON.parse(sort);
+                } catch (e) {
+                    console.error('Error parsing sort into object');
+                }
+            }
+            const comparator = (a, b) => a[sort.key].localeCompare(b[sort.key]);
+
+            response = [sortObject(response, comparator, sort)];
+        }
+
+        return response;
     } catch (err) {
         console.error(err);
         throw err;
